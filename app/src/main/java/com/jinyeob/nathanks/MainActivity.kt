26b99451df1f105830b2e816bc325b59de0c3ca4
nathankim0 <jinyeob07@gmail.com>
2023-03-10 18:00:59 +0900

    feat: app 모듈 설정

--- app/src/main/java/com/jinyeob/nathanks/MainActivity.kt
+++ app/src/main/java/com/jinyeob/nathanks/MainActivity.kt
@@ -2,10 +2,19 @@ package com.jinyeob.nathanks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jinyeob.nathanks.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.textView.text = "(changed text) Hello my name is Nathan Kim."
+
+        setContentView(binding.root)
     }
 }
